a59bfef9bba4cab6ef72bb132bf0feb9431f29fb
Moisés Medeiros <52640097+Moises788@users.noreply.github.com>
2022-05-02 00:24:58 -0300

    Add files via upload

--- Questão1/byteToMatriz.c
+++ Questão1/byteToMatriz.c
@@ -4,7 +4,7 @@
 
 unsigned char byteToDec( unsigned char codeBit){
   unsigned char bit, auxiliar;
-  int aux, numero = 0;
+  int numero = 0;
   bit=128;
 
   for(int cont=7; cont>=0; cont--){
@@ -12,47 +12,77 @@ unsigned char byteToDec( unsigned char codeBit){
     if((codeBit & bit) > 0){ 
       numero += pow(2, cont);
     }
-    else{
-      aux = 0;
-    }
     bit = bit>>1;
-    aux++;
   }
   
   return numero;
 }
 
+int setmatriz(int linhas, int colunas, unsigned char **matriz){
+    matriz = malloc(linhas * sizeof(unsigned char*));
+    for(int i = 0; i < linhas; i++){
+        matriz[i] = malloc(colunas * sizeof(unsigned char));
+    }
 
-
-int main(){
-    unsigned char *pc, matriz[8];
-    int aux = 7;
-    pc = malloc (8 * sizeof (int));
-    pc = 0;
-    
-    for(int i = 7; i >= 0; i--){
-        
-        if ((i%2) == 0){
+  for(int i = 0; i < linhas; i++) {
+		for(int j = 0; j < colunas; j++) {
+			if ((j%2) == 0){
             
-            matriz[i] = 1;
+            matriz[i][j] = 1;
         }
 
         else{
-            matriz[i] = 1;
+            matriz[i][j] = 0;
         }
-        
+		}
+	}
+
+  return matriz;
+}
+
+void imprimematriz(unsigned char **matriz, int linhas, int colunas){
+  printf("---------------------------- Matriz Gerada ---------------------------------\n\n");
+  for(int i = 0; i < linhas; i++) {        
+		for(int j = 0; j < colunas; j++){
+			printf("%d ", matriz[i][j]);
+		}
+		printf("\n\n");
+	}
+}
+
+
+int main(){
+    unsigned char *pc, **matriz;
+    unsigned __int64 decodeBit;
+    int aux = 7;
+
+    pc = malloc(8*sizeof(unsigned char*));
+    //decodeBit = malloc(sizeof(unsigned long long*));
+    matriz = setmatriz(8,8, matriz);
+    
+    for (int i = 0; i < 8; i++){
+      pc[i] = 0;
     }
-
-     for(int i = 0; i < 8; i++) {        
-        pc += byteToDec(matriz[i]<<aux);
-        aux--;
-        
-	} 
-
+   
     
-    
-    printf("%d", byteToDec(pc));
+    imprimematriz(matriz,8,8);
 
+    for (int i = 0; i < 8; i++){
+      aux =7;
+      for(int j = 0; j < 8; j++) {        
+        pc[i] += byteToDec(matriz[i][j]<<aux);
+        aux--;  
+	    } 
+    }
+    for (int i = 0; i < 8; i++){
+       printf("%d \n",pc[i]);
+    }
+   
+    memcpy (&decodeBit, pc, sizeof (unsigned __int64));
+
+    printf("%d \n", decodeBit);
+   
+    
     
     return 0;
 }
