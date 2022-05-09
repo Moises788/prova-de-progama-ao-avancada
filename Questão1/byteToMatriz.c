94e027d142bfc206c55ffc45a1e798667027dc01
Moisés Medeiros <52640097+Moises788@users.noreply.github.com>
2022-05-08 21:08:08 -0300

    Add files via upload

--- Questão1/byteToMatriz.c
+++ Questão1/byteToMatriz.c
@@ -1,16 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
 
-unsigned char byteToDec( unsigned char codeBit){
-  unsigned char bit, auxiliar;
-  int numero = 0;
+unsigned char decToByte( unsigned char codeBit){
+  unsigned char bit;
+  int numero = 0, cont;
   bit=128;
 
-  for(int cont=7; cont>=0; cont--){
+  for(cont=7; cont>=0; cont--){
 
-    if((codeBit & bit) > 0){ 
-      numero += pow(2, cont);
+    if((codeBit & bit) > 0){
+      printf("1");
+    }
+    else{
+      printf("0");
     }
     bit = bit>>1;
   }
@@ -18,32 +20,11 @@ unsigned char byteToDec( unsigned char codeBit){
   return numero;
 }
 
-int setmatriz(int linhas, int colunas, unsigned char **matriz){
-    matriz = malloc(linhas * sizeof(unsigned char*));
-    for(int i = 0; i < linhas; i++){
-        matriz[i] = malloc(colunas * sizeof(unsigned char));
-    }
-
-  for(int i = 0; i < linhas; i++) {
-		for(int j = 0; j < colunas; j++) {
-			if ((j%2) == 0){
-            
-            matriz[i][j] = 1;
-        }
-
-        else{
-            matriz[i][j] = 0;
-        }
-		}
-	}
-
-  return matriz;
-}
-
-void imprimematriz(unsigned char **matriz, int linhas, int colunas){
+void imprimematriz(unsigned char matriz[8][8] , int linhas, int colunas){
   printf("---------------------------- Matriz Gerada ---------------------------------\n\n");
-  for(int i = 0; i < linhas; i++) {        
-		for(int j = 0; j < colunas; j++){
+  int i, j;
+  for(i = 0; i < linhas; i++) {        
+		for(j = 0; j < colunas; j++){
 			printf("%d ", matriz[i][j]);
 		}
 		printf("\n\n");
@@ -52,37 +33,45 @@ void imprimematriz(unsigned char **matriz, int linhas, int colunas){
 
 
 int main(){
-    unsigned char *pc, **matriz;
-    unsigned __int64 decodeBit;
+    unsigned char *pc, matriz[8][8];
+    unsigned long long decodeBit;
+    int i, j;
     int aux = 7;
+  for(i = 0; i < 8; i++) {
+		for(j = 0; j < 8; j++) {
+			if ((j%2) == 0){
+            
+            matriz[i][j] = 0;
+        }
 
-    pc = malloc(8*sizeof(unsigned char*));
-    //decodeBit = malloc(sizeof(unsigned long long*));
-    matriz = setmatriz(8,8, matriz);
-    
-    for (int i = 0; i < 8; i++){
-      pc[i] = 0;
-    }
-   
-    
+        else{
+            matriz[i][j] = 1;
+        } 
+		}
+	}
+    pc = &decodeBit;
+   matriz[0][1] = 0;
+   matriz[0][0] = 0;
     imprimematriz(matriz,8,8);
 
-    for (int i = 0; i < 8; i++){
+    for ( i = 0; i < 8; i++){
       aux =7;
-      for(int j = 0; j < 8; j++) {        
-        pc[i] += byteToDec(matriz[i][j]<<aux);
+      pc[i] = 0;
+      
+      for( j = 0; j < 8; j++) {        
+        pc[i] += matriz[i][j]<<aux;
         aux--;  
-	    } 
+	    }
+      printf("%d \n", pc[i]); 
+       
     }
-    for (int i = 0; i < 8; i++){
-       printf("%d \n",pc[i]);
-    }
-   
-    memcpy (&decodeBit, pc, sizeof (unsigned __int64));
 
-    printf("%d \n", decodeBit);
+    printf("%llu \n", decodeBit);
+
+     for(i=0; i<=7; i++){
+    decToByte(pc[i]);
+    printf("|");
+  }
    
-    
-    
     return 0;
 }
