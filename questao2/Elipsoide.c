113134f3fb83c6c1da5f0b93651c7077b4e19773
Moisés Medeiros <52640097+Moises788@users.noreply.github.com>
2022-05-12 10:58:26 -0300

    resposta das provas completas

--- questao2/Elipsoide.c
+++ questao2/Elipsoide.c
@@ -1,44 +1,106 @@
+/*
+  Questão: 2 - prova de ponteiros
+  Aluno: Moisés de Medeiros Galvão
+  PROGRAMACAO AVANCADA - T01 
+  Professor: AGOSTINHO DE MEDEIROS BRITO JUNIOR
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-int main(void) {
-  unsigned char ***mat;
-  int i, j, k, x=11, y=11, z=13;
-  int xcenter = 5, ycenter =5, zcenter=6, rx=5, ry=5, rz=6;
-  int planoz;
-  
-  mat = malloc (sizeof(int**)*x);
+unsigned char ***setSpace(unsigned char ***matriz, int x, int y, int z);
+void freeMatriz(unsigned char ***matriz, unsigned int x, unsigned int y);
+void sendPlan(unsigned char ***matriz, unsigned int x, unsigned int y, unsigned int z);
 
-  for(i =0; i<x; i++){
-    mat[i] = malloc(sizeof(int*)*y);
-    for (j=0; j<y; j++){
-      mat[i][j] = malloc(sizeof(int)*z);
-      }
+int main(void) {
+  unsigned char ***matriz;
+  unsigned int planoz, x, y, z;
+  
+  printf("digite as dimensões do espaço tridimensional: \n");
+  printf("obs: os eixos x, y e z devem ser impares para que a elipsóide toque os extremos do espaço.\n ");
+
+  printf("eixo x:\n ");
+  scanf("%d", &x);
+  
+  printf("eixo y:\n ");
+  scanf("%d", &y);
+  
+  printf("eixo z:\n ");
+  scanf("%d", &z);
+
+  //enviando a elipsoide para a matriz
+  matriz = setSpace(matriz, x, y, z);
+
+  printf("Digite a posição\n\n");
+  scanf("%d", &planoz);
+
+  //cortando a elipsoide no plano z escolhido e visualizando a figura em vista superior
+  sendPlan(matriz, x, y, planoz );
+  freeMatriz( matriz, x, y); //libera memória alocada
+ 
+}
+
+//função que realia o cálculo necessário para formar a elipsóide
+unsigned char ***setSpace(unsigned char ***matriz, int x, int y, int z){
+  int centro_x, centro_y, centro_z; 
+  //Calculo para encontrar o centro de cada eixo
+  centro_x = (x-1)/2;
+  centro_y = (y-1)/2;
+  centro_z = (z-1)/2;
+  
+  //Alocação dinâmica dos espaço tridimencional
+  matriz = malloc (sizeof(int**)*x);
+  for(int i =0; i<x; i++){
+    matriz[i] = malloc(sizeof(int*)*y);
+    for (int j=0; j<y; j++){
+      matriz[i][j] = malloc(sizeof(int)*z);
+    }
   } 
-  for(i =0; i<x; i++){
-    for (j=0; j<y; j++){
-      for(k=0; k<z; k++){
-        //mat[i][j][k]  = i+j+k;
-        if (((pow(i-xcenter,2)/pow(rx, 2)) + (pow(j-ycenter,2)/pow(ry,2))+(pow(k-zcenter, 2)/pow(rz, 2))) <= 1){
-          mat[i][j][k]  = 1;
+
+//impementação da fórmula da elipsóide
+//Referência: indície 1.2 do site:  //http://www.mat.ufpb.br/~sergio/winplot/vetorial/quadricas.html#:~:text=1.2.-,Elips%C3%B3ide,os%20eixos%20maior%20e%20menor.
+  for(int i =0; i<x; i++){
+    for (int j=0; j<y; j++){
+      for(int k=0; k<z; k++){
+        if (((pow(i-centro_x,2)/pow(centro_x, 2)) + (pow(j-centro_y,2)/pow(centro_y,2))+(pow(k-centro_z, 2)/pow(centro_z, 2))) <= 1){
+          matriz[i][j][k]  = 'X'; //Preenchend o formato da elipsóide com um 'X'
         }
         else{
-          mat[i][j][k]  = 0;
+          matriz[i][j][k]  = 'o'; //espaços vazios representados com um 'o'
         }
           
-        }
-        
-        }
       }
-  printf("Digite a posição\n\n");
+        
+    }
+  }
 
-  scanf("%d", &planoz);
- for(i =0; i<x; i++){
-    for (j=0; j<y; j++){
-      printf("%d", mat[i][j][planoz]);
+  return matriz;
+  freeMatriz( matriz, x, y); //liberando memória
+  
+}
+
+//função para impressão da matriz a partir de um plano z (vista superior da figura)
+void sendPlan(unsigned char ***matriz, unsigned int x, unsigned int y, unsigned int z){
+  for(int i =0; i<x; i++){
+    for (int j=0; j<y; j++){
+      printf("%c", matriz[i][j][z]);
         }
    printf("\n");
       }
-  
+}
+//função que liberar a memória alocada
+void freeMatriz(unsigned char ***matriz, unsigned int x, unsigned int y){
+  //liberando o eixo z
+  for ( int i=0 ; i<x ; i++){
+      for ( int j=0; j<y ; j++){
+      free( matriz[i][j] );
+    }
+  }
+  //liberando o eixo y
+  for ( int i=0 ; i<x ; i++ ){
+  free ( matriz [i] );
+  }
+  //liberando o eixo x
+  free( matriz );
 }
