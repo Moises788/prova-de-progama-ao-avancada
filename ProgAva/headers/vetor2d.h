778325849a8116b015b3eefd5b43bbff1d685270
Moisés Medeiros <52640097+Moises788@users.noreply.github.com>
2022-05-23 15:40:18 -0300

    aprendendo poo

---
+++ ProgAva/headers/vetor2d.h
@@ -0,0 +1,13 @@
+#ifndef VETOR2D_H
+#define VETOR2D_H
+
+class Vetor2d
+{
+private:
+    float x, y;
+public:
+    void setX(float x_);
+    float getX(void);
+};
+
+#endif
---
+++ ProgAva/soucers/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "vetor2d.h"
+
+int main(){
+    Vetor2d v;
+    v.setX(2);
+
+    std::cout << v.getX();
+}
